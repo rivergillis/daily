a26d27e113e30e38547d3463b17a53c0e1f926fd
River Gillis <jrivergillis@gmail.com>
2016-09-07 10:05:31 -0500

    Dirty implementation for small conversion from decimal to base fib

--- 282.py
+++ 282.py
@@ -1,3 +1,4 @@
+#https://www.reddit.com/r/dailyprogrammer/comments/5196fi/20160905_challenge_282_easy_unusual_bases/
 #generate the nth fibonacci number
 def fib():
     a,b = 0,1
@@ -12,8 +13,26 @@ for i,n in enumerate(fib()):
     if i == 1000:
         break
 
+# convert from decimal base to base fib
 def dtofib(n):
     print("dtofib")
+    remainders = []
+    # find the highest divisible fib
+    i = 20
+    while i > 0:
+        print("i: " + str(i) + " fiblist[i]: " + str(fiblist[i]) + " n: " + str(n))
+        if n >= fiblist[i]:
+            print(str(n) + " is greater than equal to " +  str(fiblist[i]))
+            remainders.append(1)
+            n -= fiblist[i]
+        else:
+            print(str(n) + " is not greater than equal to " + str(fiblist[i]))
+            remainders.append(0)
+        i -= 1
+    return remainders
+
+
+# convert from base fib to decimal base
 def fibtod(s):
     print("fibtod")
     total = 0
@@ -24,9 +43,10 @@ def fibtod(s):
     #print("about to return! " + str(total))
     return total
 
+# gather input, determine what the base is, send to the function
 inputs = input().split(' ')
 if inputs[0] == "10":
-    print(str(dtofib((inputs[1]))))
+    print(str(dtofib((int(inputs[1])))))
 else:
     print(str(fibtod((inputs[1]))))
 
