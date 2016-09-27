3f2a640a1e9344e60e682579fa2903675b4468db
River Gillis <jrivergillis@gmail.com>
2016-09-26 22:29:05 -0500

    implement all problem requirements for 284

--- 284.py
+++ 284.py
@@ -4,26 +4,31 @@ with(open(WORDLIST_FILENAME, 'r')) as f:
 # content is now a list of every word in words.txt
 print("Loaded " + str(len(content)) + " words")
 
-inputString = "bhapple"
+inputString = input("Please 'swype' something: ")
 possible = []
 for word in content:
-    tempString = inputString
-    possibleFlag = True
-    for letter in word:
-        if letter not in tempString:
-            possibleFlag = False
-            if (word == 'apple'):
-                print(letter + " is not in " + tempString)
-        else:
-            if (word == 'apple'):
-                print(str(len(tempString)) + " " + tempString + " " + str(inputString.index(letter)))
-            if (len(tempString) - 1) != tempString.index(letter):
-                tempString = tempString[tempString.index(letter)+1:]
-            else:
-                tempString = ''
+    if len(word) >= 5:
+        prevLetter = ""
+        tempString = inputString
+        possibleFlag = True
+        for letter in word:
+            if letter != prevLetter: # get double letters for free
+                if letter not in tempString:
+                    possibleFlag = False
+                    #if (word == 'apple'):
+                    #    print(letter + " is not in " + tempString)
+                else:
+                    #if (word == 'apple'):
+                    #    print(str(len(tempString)) + " " + tempString + " " + str(inputString.index(letter)))
+                    if (len(tempString) - 1) != tempString.index(letter):
+                        tempString = tempString[tempString.index(letter)+1:]
+                    else:
+                        tempString = ''
+                    prevLetter = letter
                 
             #tempString = tempString.replace(letter, "")
-    if possibleFlag:
-        possible.append(word)
-        print("**************FOUND " + word)
-print(possible)
+        if possibleFlag:
+            possible.append(word)
+            print("**************FOUND " + word)
+betterPicks = [x for x in possible if x[0] == inputString[0] and x[-1] == inputString[-1]]
+print(betterPicks)
